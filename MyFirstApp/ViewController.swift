6318006db8aaaebcf3ef1918e0a05d00c7a6dcad
Eyüp Fidan <eyupfidanweb@gmail.com>
2021-12-10 00:15:02 +0300

    Swift yaptığım ilk basit app.

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var MyLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        ImageView.image = UIImage(named: "pera-2")        
+    }
+    
 }
 
